b499b4bbbae6b07105040ac3c69062137fbf28cd
王琼 <qiong.wang@upai.com>
2022-05-17 10:05:33 +0800

    修复多斜杠url匹配问题

--- src/swagger/index.js
+++ src/swagger/index.js
@@ -43,6 +43,8 @@ class Swagger {
 
   getPath (url) {
     return _.find(this.pathObjects, (pathObject) => {
+      // The / at the end will match successfully in the express routing rule base
+      url = url.replace(new RegExp(`${this.basePathPrefix}/{1,2}`), `${this.basePathPrefix}/`)
       return _.isArray(pathObject.regexp.exec(url))
     })
   }
